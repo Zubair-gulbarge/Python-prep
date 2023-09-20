71b6faca3e12922fb30507d016eebdda9957ccfe
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-09-20 19:14:10 +0530

    Update matplot2.py

--- matplot2.py
+++ matplot2.py
@@ -1,37 +1,37 @@
-Sure, let's go through a simple Matplotlib exercise. In this exercise, we'll create a basic line plot with some customization. We'll use Python's Matplotlib library to visualize a set of data points.
+# Sure, let's go through a simple Matplotlib exercise. In this exercise, we'll create a basic line plot with some customization. We'll use Python's Matplotlib library to visualize a set of data points.
 
-**Exercise: Creating a Line Plot**
+# **Exercise: Creating a Line Plot**
 
-In this exercise, we'll create a line plot to visualize the growth of a fictional company's revenue over several years. Follow these steps:
+# In this exercise, we'll create a line plot to visualize the growth of a fictional company's revenue over several years. Follow these steps:
 
-1. Import Matplotlib and create a list of years and corresponding revenue data.
+# 1. Import Matplotlib and create a list of years and corresponding revenue data.
 
-2. Create a line plot using Matplotlib to display the revenue data over the years.
+# 2. Create a line plot using Matplotlib to display the revenue data over the years.
 
-3. Customize the plot by adding a title, labels for the x and y axes, and a legend.
+# 3. Customize the plot by adding a title, labels for the x and y axes, and a legend.
 
-4. Save the plot as an image file (e.g., PNG).
+# 4. Save the plot as an image file (e.g., PNG).
 
-Here's a template for the exercise:
+# Here's a template for the exercise:
 
-```python
-import matplotlib.pyplot as plt
+# ```python
+# import matplotlib.pyplot as plt
 
-# Step 1: Define the data
-years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
-revenue = [50000, 60000, 75000, 90000, 110000, 130000, 160000, 180000, 200000, 220000, 240000]
+# # Step 1: Define the data
+# years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
+# revenue = [50000, 60000, 75000, 90000, 110000, 130000, 160000, 180000, 200000, 220000, 240000]
 
-# Step 2: Create the line plot
-# Your code here
+# # Step 2: Create the line plot
+# # Your code here
 
-# Step 3: Customize the plot
-# Your code here
+# # Step 3: Customize the plot
+# # Your code here
 
-# Step 4: Save the plot as an image
-# Your code here
+# # Step 4: Save the plot as an image
+# # Your code here
 
-# Display the plot
-plt.show()
-```
+# # Display the plot
+# plt.show()
+# ```
 
-Now, complete the exercise by filling in the code for creating the line plot, customizing it, and saving it as an image. You can customize the plot with your own title, axis labels, and legend labels. Once you've completed the exercise, you'll have a basic line plot visualizing the company's revenue growth over the years.
+# Now, complete the exercise by filling in the code for creating the line plot, customizing it, and saving it as an image. You can customize the plot with your own title, axis labels, and legend labels. Once you've completed the exercise, you'll have a basic line plot visualizing the company's revenue growth over the years.
