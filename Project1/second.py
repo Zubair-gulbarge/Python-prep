3dd1a5e880400b10feba8ad0243c9a1992790ebd
Zubair-gulbarge <gmdzubair143@gmail.com>
2023-09-23 21:47:00 +0530

    Update second.py

---
+++ Project1/second.py
@@ -0,0 +1,58 @@
+# Of course! Here are a few more practice data analysis problems along with solutions:
+
+# **Problem 1: Analyzing Student Exam Scores**
+
+# **Dataset Description:**
+# You have a dataset containing information about student exam scores. The dataset includes the following columns:
+# - `StudentID`: Unique identifier for each student.
+# - `Name`: Student's name.
+# - `Math_Score`: Math exam score.
+# - `Science_Score`: Science exam score.
+# - `English_Score`: English exam score.
+
+# **Tasks:**
+# 1. Load the dataset and explore it.
+# 2. Calculate the average score for each subject.
+# 3. Find the student(s) with the highest score in each subject.
+# 4. Calculate the overall average score for all students.
+# 5. Create visualizations to compare the distribution of scores in different subjects.
+
+# **Solution:**
+# This problem can be solved using Python and libraries like pandas and matplotlib. You can load the dataset, calculate statistics, and create visualizations similar to the previous example.
+
+# **Problem 2: Analyzing Customer Purchase Data**
+
+# **Dataset Description:**
+# You have a dataset containing information about customer purchases at an online store. The dataset includes the following columns:
+# - `CustomerID`: Unique identifier for each customer.
+# - `Date`: Date of the purchase.
+# - `Product`: Name of the product purchased.
+# - `Price`: Price of the product.
+# - `Quantity`: Number of units of the product purchased.
+
+# **Tasks:**
+# 1. Load the dataset and explore it.
+# 2. Calculate the total revenue generated from these sales.
+# 3. Find the top-selling product(s).
+# 4. Determine the total number of customers and the average purchase per customer.
+# 5. Visualize the distribution of purchase prices.
+
+# **Solution:**
+# Similar to the previous problem, you can use Python, pandas, and matplotlib to load the dataset, perform calculations, and create visualizations.
+
+# **Problem 3: Analyzing Stock Market Data**
+
+# **Dataset Description:**
+# You have a dataset containing historical stock market data for a particular company. The dataset includes columns like `Date`, `Open`, `High`, `Low`, `Close`, and `Volume`.
+
+# **Tasks:**
+# 1. Load the dataset and explore it.
+# 2. Calculate the average closing price over a specific time period (e.g., monthly or yearly).
+# 3. Identify days with the highest trading volume (unusually high volume).
+# 4. Calculate the percentage change in closing prices from one day to the next.
+# 5. Visualize stock price trends and trading volume over time.
+
+# **Solution:**
+# For this problem, you can use Python libraries like pandas, numpy, and matplotlib to analyze the stock market data, perform calculations, and create various types of charts to visualize the data.
+
+# # Feel free to choose any of these practice problems, and if you'd like to see a more detailed solution for any specific problem, please let me know, and I can provide it.
